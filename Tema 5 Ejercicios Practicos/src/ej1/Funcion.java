9cc32b6eb8aef2b483ae7ce71daf18d2ee82ccd3
jsevilla202 <jsevilla@iesnervion.es>
2023-02-08 11:06:41 +0100

    Correccion de errores y comentado

--- Tema 5 Ejercicios Practicos/src/ej1/Funcion.java
+++ Tema 5 Ejercicios Practicos/src/ej1/Funcion.java
@@ -4,54 +4,82 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Funcion {
+	//Funcion minas
 	char[] minas(char[] t) {
+		//Crea una tabla con la misma longitud que la recibida
 		char tabla[] = new char [t.length];
-		final int limite = 6;
+		//Establecemos un parametro para el límite de minas
+		final int minas = 6;
+		//Parametro que recoge la posición aleatoria en la tabla
 		int pos;
-		for(int i = 0; i<limite; i++) {
+		//Rellenamos aleatoriamente celdas con minas
+		for(int i = 0; i<minas; i++) {
 			pos = (int) (Math.random()*20);
-			
+			//En caso de que en la celda escogida haya una mina, retrocede el contador
 			if(tabla[pos] == '*') {
 				i--;
 			}
+			//Si no es el caso, hace lo siguiente
 			else {
+				//Pone la mina
 				tabla[pos] = '*';
+				//Comprueba que no esté en la ultima posición
 				if(pos!=tabla.length-1) {
+					//Si no lo está, comprueba que no haya una mina en la posición siguiente
 					if(tabla[pos+1] != '*') {
+						//Si no la hay coloca en 1
 						tabla[pos+1] = '1';
 					}
 				}
+				//Comprueba que no esté en la primera posición
 				if(pos!=0) {
+					//Si no lo está, comprueba que no haya una mina en la posición anterior
 					if(tabla[pos-1] != '*') {
+						//Si no la hay coloca en 1
 						tabla[pos-1] = '1';
 					}
 			}
 		}
 		}
-		
+		//Con este bucle corregiremos el error de 1 en la celdas rodeadas por minas
 		for(int i = 1; i<tabla.length-1; i++) {
+			//Comprueba que en la posición anterior y en la siguiente haya una mina y que en la posición no haya una mina
 			if(tabla[i-1]=='*'&&tabla[i+1]=='*'&&tabla[i]!='*') {
+				//Si se cumplen los requisitos, cambia el 1 por el 2
 				tabla[i]='2';
 			}
 		}
+		//Rellena los huecos vacios con 0
 		for(int i = 0; i<tabla.length; i++) {
+			//Comprueba que en esa posición no haya una mina, un 1 o un 2
 			if(tabla[i]!='*'&&tabla[i]!='1'&&tabla[i]!='2') {
+				//Si se cumplen los requisitos, pone un 0
 				tabla[i]='0';
 			}
 		}
-		System.out.println(Arrays.toString(tabla));
+		//Devuelve la tabla
 		return tabla;
 	}
 	
+	//Funcion juego
 	void juego(char[] t) {
+		//Crea una tabla con la misma longitud que la recibida, esta tabla va a ser la que el usuario vea
 		char[] tabla = new char [t.length];
+		//La rellena con simbolos de interrogación
 		Arrays.fill(tabla, '?');
+		//Parametro que comprueba si el jugador ha muerto
 		boolean muerto = false;
+		//Parametro que recoge la posición introducida por el usuario
 		int pos;
+		//Contador
 		int i = 0;
+		//crea un Scanner y lo nombra "sc"
 		Scanner sc = new Scanner(System.in);
 		
+		//Bucle del juego
 		do {
+			System.out.println();
+			System.out.println(Arrays.toString(tabla));
 			if(i==14) {
 				break;
 			}
@@ -64,10 +92,10 @@ public class Funcion {
 				break;
 			}
 			tabla[pos] = t[pos];
-			System.out.println(Arrays.toString(tabla));
 			i++;
 		}
 		while(t[pos]!='*');
+		
 		sc.close();
 		System.out.println();
 		System.out.println(muerto? "\033[0;1m" + "\033[31;1;1mPERDISTES, UNA MINA TE HA MATADO\033[0m" : "\033[0;1m" + "\033[32;1;1mENHORABUENA, HAS GANADO!!\033[0m");
--- Tema 5 Ejercicios Practicos/src/ej1/Main.java
+++ Tema 5 Ejercicios Practicos/src/ej1/Main.java
@@ -3,15 +3,19 @@ package ej1;
 public class Main {
 
 	public static void main(String[] args){
+		//Crea una tabla de longitud 20
 		char  tabla[] = new char [20];
-		
+		//Importa las funciones de la clase Funcion
 		final Funcion funcion = new Funcion();
 		
+		//Iguala la tabla principal a la tabla que devuelve el metodo minas
 		tabla = funcion.minas(tabla);
 		
+		//Introduce al usuario en el juego
 		System.out.println("Bienvenid@ al juego de las minas, el juego es muy sencillo\n 1. Inserta un número del 1 al 20"+
 		"\n 2. Si recibes un número, el 0 o el 1 o el 2, significa que ceca hay esa cantidad de minas"+
 				"\n 3. Si sale una bomba, has perdido\nPasalo bien jugando, el juego empezara automaticamente");
+		//Introduce la tabla obtenida en el juego
 		funcion.juego(tabla);
 
 	}
