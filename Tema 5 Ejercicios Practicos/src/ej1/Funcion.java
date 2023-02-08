e1ab5de6d3c187b0fc1ab07eda0b5a7f09c3990c
meganano202 <meganano202@192.168.1.134>
2023-02-08 13:41:01 +0100

    Función terminada y comentada

--- Tema 5 Ejercicios Practicos/src/ej1/Funcion.java
+++ Tema 5 Ejercicios Practicos/src/ej1/Funcion.java
@@ -70,34 +70,50 @@ public class Funcion {
 		//Parametro que comprueba si el jugador ha muerto
 		boolean muerto = false;
 		//Parametro que recoge la posición introducida por el usuario
-		int pos;
-		//Contador
-		int i = 0;
+		int pos = 0;
+		//Como no se puede inicializar a un valor que contenga una mina, irá sumando hasta encontrar un valor que no la contenga
+		while (t[pos] == '*') {
+		    pos++;
+		}
 		//crea un Scanner y lo nombra "sc"
 		Scanner sc = new Scanner(System.in);
 		
-		//Bucle del juego
-		do {
-			System.out.println();
-			System.out.println(Arrays.toString(tabla));
-			if(i==14) {
-				break;
-			}
-			System.out.println();
-			System.out.print("Inserta un número del 1 al 20: ");
-			pos = sc.nextInt();
-			pos--;
-			if(t[pos]=='*') {
-				muerto = true;
-				break;
-			}
-			tabla[pos] = t[pos];
-			i++;
+		//Un bucle que se repetira 14 veces o hasta que toque una mina
+		for (int i = 0; i < 14 && t[pos] != '*'; i++) {
+		    //Espacio
+		    System.out.println();
+		    //Imprime la tabla con los ?
+		    System.out.println(Arrays.toString(tabla));
+		    //Espacio
+		    System.out.println();
+		    //Le pide al usurio que inserte un número del 1 al 20
+		    System.out.print("Inserta un número del 1 al 20: ");
+		    //Guarda la respuesta en el parametro pos
+		    pos = sc.nextInt();
+		    //Si ha introducido un valor entre 1 y 20 continua
+		    if(pos >= 1 && pos <= 20) {
+		        //Resta 1 al valor de posición para ajustarlo al array
+		        pos--;
+		        //Si el valor del array en esa psición es igual a una mina
+		        if(t[pos] == '*') {
+		            //El booleano pasara a ser verdadero y saldrá con un break
+		            muerto = true;
+		        } else {
+		            //Se iguala la posición de la tabla con ? con la que tiene el resultado paa mostrar las casillas ya usadas
+		            tabla[pos] = t[pos];
+		        }
+		    } else {
+		        //Muestra un mensaje de error
+		        System.out.println("ERROR: Introduzca un número del 1 al 20");
+		        i--;
+		    }
 		}
-		while(t[pos]!='*');
-		
+
+		//Cierra el scanner
 		sc.close();
+		//Espacio
 		System.out.println();
+		//Mensaje de victoria o derrta dependiendo del booleano muerto
 		System.out.println(muerto? "\033[0;1m" + "\033[31;1;1mPERDISTES, UNA MINA TE HA MATADO\033[0m" : "\033[0;1m" + "\033[32;1;1mENHORABUENA, HAS GANADO!!\033[0m");
 	}
 	
